32c89bd00906d0fbf901dda3d03b6a7e786c312d
kirill43qw <default11user@gmail.com>
2025-06-06 13:47:38 +0300

    removed ruff from lsp. Configured pyright

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,10 +1,8 @@
-require("nvchad.configs.lspconfig").defaults()
-
 local lspconfig = require "lspconfig"
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- local servers = { "html", "pyright", "docker-compose-language-service", "dockerfile-language-server", "lua-language-server"}
-local servers = {"lua_ls", "dockerls", "pyright", "ruff" }
+local servers = {"lua_ls", "dockerls", "pyright",}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -22,38 +20,11 @@ lspconfig.pyright.setup({
   settings = {
     pyright = {
       autoImportCompletion=true,
-      -- disableOrganizeImports = true,
     },
     python = {analysis = {
-      ignore = { '*' },
       autoSearchPaths=true,
-      -- diagnosticMode='openFilesOnly',
-      -- userLibraryCodeForTypes=true,
-      typeCheckingMode='off'
+      diagnosticMode='openFilesOnly',
+      typeCheckingMode='basic'
     }},
   },
 })
-
-lspconfig.ruff.setup({
-  on_attach = nvlsp.on_attach,
-  capabilities = nvlsp.capabilities,
-  on_init = nvlsp.on_init,
-  init_options = {
-    settings = {
-      args = { "--line-length=88" }, -- Пример дополнительных аргументов
-    },
-  },
-})
-
--- vim.lsp.config('ruff', {
---   on_attach = nvlsp.on_attach,
---   capabilities = nvlsp.capabilities,
---   on_init = nvlsp.on_init,
---   init_options = {
---     settings = {
---       args = { "--line-length=88" }, -- Пример дополнительных аргументов
---     }
---   }
--- })
--- --
--- vim.lsp.enable('ruff')
