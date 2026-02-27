44c6542fbf6fec83c93cc11ac08dbc6fc87e52b3
kirill43qw <default11user@gmail.com>
2026-02-27 10:27:47 +0300

    fix lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -12,7 +12,11 @@ for _, lsp in ipairs(servers) do
 end
 
 vim.lsp.config("pyright", {
-  on_attach = nvlsp.on_attach,
+  -- on_attach = nvlsp.on_attach,
+  on_attach = function(client, bufnr) -- убрать всплывающие подсказки от lsp
+    nvlsp.on_attach(client, bufnr)
+    client.server_capabilities.signatureHelpProvider = nil
+  end,
   capabilities = nvlsp.capabilities,
   on_init = nvlsp.on_init,
   filetypes = { "python" },
